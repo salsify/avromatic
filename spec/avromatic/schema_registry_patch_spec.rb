445466e45d73d876e93161d7af73274eab374d0f
Tim Perkins <tjwp@users.noreply.github.com>
2017-02-01 09:33:45 -0500

    Add use_cacheable_schema_registration configuration (#50)

--- spec/avromatic/schema_registry_patch_spec.rb
+++ spec/avromatic/schema_registry_patch_spec.rb
@@ -32,5 +32,17 @@ describe AvroTurf::SchemaRegistry, 'schema registry patch' do
       expect(registry.register(subject_name, avro_schema)).to eq(id)
       expect(FakeSchemaRegistryServer).not_to have_received(:post)
     end
+
+    context "when use_cacheable_schema_registration is false" do
+      before do
+        allow(Avromatic).to receive(:use_cacheable_schema_registration).and_return(false)
+      end
+
+      it "does not check that the schema exists before attempting to register" do
+        allow(registry).to receive(:get).and_call_original
+        registry.register(subject_name, avro_schema)
+        expect(registry).not_to have_received(:get)
+      end
+    end
   end
 end
