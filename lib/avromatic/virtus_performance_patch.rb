19363ea806a584eec36c4ab792bed50ace28483a
Will Leonard <wleonard@salsify.com>
2017-07-31 19:37:05 -0400

    Some more

--- lib/avromatic/virtus_performance_patch.rb
+++ lib/avromatic/virtus_performance_patch.rb
@@ -28,38 +28,38 @@ module VirtusAllowedInstanceMethodsMemoization
 end
 
 Virtus::InstanceMethods.prepend(VirtusAllowedInstanceMethodsMemoization)
-
-module VirtusAllowedWriterMethodsExtensionMemoization
-  def self.prepended(base)
-    class << base
-      prepend ClassMethods
-    end
-  end
-
-  module ClassMethods
-    def included(base)
-      # puts 'hi from VirtusAllowedWriterMethodsExtensionMemoization'
-      if base.class == Module
-        base.prepend(VirtusAllowedWriterMethodsExtensionMemoization)
-      else
-        base.class_eval do
-          class_attribute :allowed_writer_methods_extensions
-        end
-      end
-      super
-    end
-  end
-
-  def allowed_writer_methods
-    if (self.is_a?(Class) && self.config.mutable) #|| (!self.is_a?(Class) && self.class.config.mutable)
-      super
-    else
-      self.allowed_writer_methods_extensions ||= super
-    end
-  end
-end
-
-Virtus::Extensions::AllowedWriterMethods.prepend(VirtusAllowedWriterMethodsExtensionMemoization)
+#
+# module VirtusAllowedWriterMethodsExtensionMemoization
+#   def self.prepended(base)
+#     class << base
+#       prepend ClassMethods
+#     end
+#   end
+#
+#   module ClassMethods
+#     def included(base)
+#       # puts 'hi from VirtusAllowedWriterMethodsExtensionMemoization'
+#       if base.class == Module
+#         base.prepend(VirtusAllowedWriterMethodsExtensionMemoization)
+#       else
+#         base.class_eval do
+#           class_attribute :allowed_writer_methods_extensions
+#         end
+#       end
+#       super
+#     end
+#   end
+#
+#   def allowed_writer_methods
+#     if (self.is_a?(Class) && self.config.mutable) #|| (!self.is_a?(Class) && self.class.config.mutable)
+#       super
+#     else
+#       self.allowed_writer_methods_extensions ||= super
+#     end
+#   end
+# end
+#
+# Virtus::Extensions::AllowedWriterMethods.prepend(VirtusAllowedWriterMethodsExtensionMemoization)
 
 module VirtusValueObjectAllowedWriterMethodsMemoization
   def self.prepended(base)
