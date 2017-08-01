d9dc1839027c9ec4507e375158034206a2597269
Will Leonard <wleonard@salsify.com>
2017-08-01 09:30:16 -0400

    Fix rubocop

--- spec/avromatic/virtus_value_object_allowed_writer_methods_memoization_spec.rb
+++ spec/avromatic/virtus_value_object_allowed_writer_methods_memoization_spec.rb
@@ -4,8 +4,8 @@ describe VirtusValueObjectAllowedWriterMethodsMemoization do
 
   describe "#allowed_writer_methods" do
     let(:test_class) { Avromatic::Model.model(value_schema_name: 'test.encode_value') }
-    let(:instance1) { test_class.new({ str1: 'a', str2: 'b' }) }
-    let(:instance2) { test_class.new({ str1: 'c', str2: 'd' }) }
+    let(:instance1) { test_class.new(str1: 'a', str2: 'b') }
+    let(:instance2) { test_class.new(str1: 'c', str2: 'd') }
 
     it "shares the return value across instances" do
       expect(instance1.allowed_writer_methods.object_id).to eq(instance2.allowed_writer_methods.object_id)
