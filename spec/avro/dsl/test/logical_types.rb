46470b8553b888f6d79c97a353b4b0001ed40381
Igor Pstyga <igor.pstyga@gmail.com>
2023-02-10 13:31:27 -0800

    Make decimal logical type conditional by Avro::VERSION

--- spec/avro/dsl/test/logical_types.rb
+++ spec/avro/dsl/test/logical_types.rb
@@ -4,6 +4,6 @@ record :logical_types, namespace: :test do
   required :date, :int, logical_type: 'date'
   required :ts_msec, :long, logical_type: 'timestamp-millis'
   required :ts_usec, :long, logical_type: 'timestamp-micros'
-  required :decimal, :bytes, logical_type: 'decimal', precision: 4
+  required :decimal, :bytes, logical_type: 'decimal', precision: 4, scale: 2
   required :unknown, :int, logical_type: 'foobar'
 end
--- spec/support/contexts/logical_types_serialization.rb
+++ spec/support/contexts/logical_types_serialization.rb
@@ -4,7 +4,9 @@
 #   decoded: a model instance based on the encoded_value
 shared_examples_for "logical type encoding and decoding" do
   context "logical types" do
-    let(:schema_name) { 'test.logical_types' }
+    let(:schema_name) do
+      Avromatic.allow_decimal_logical_type ? 'test.logical_types_with_decimal' : 'test.logical_types'
+    end
     let(:test_class) do
       Avromatic::Model.model(schema_name: schema_name)
     end
@@ -25,9 +27,8 @@ shared_examples_for "logical type encoding and decoding" do
             date: Date.today,
             ts_msec: now,
             ts_usec: now,
-            decimal: BigDecimal('5.2'),
             unknown: 42
-          }
+          }.tap { _1[:decimal] = BigDecimal('5.2') if Avromatic.allow_decimal_logical_type }
         end
 
         it "encodes and decodes instances" do
@@ -44,9 +45,8 @@ shared_examples_for "logical type encoding and decoding" do
             date: (Date.today - epoch_start).to_i,
             ts_msec: now.to_i + now.usec / 1000 * 1000,
             ts_usec: now.to_i * 1_000_000 + now.usec,
-            decimal: BigDecimal('1.5432'),
             unknown: 42
-          }
+          }.tap { _1[:decimal] = BigDecimal('1.5432') if Avromatic.allow_decimal_logical_type }
         end
 
         it "encodes and decodes instances" do
